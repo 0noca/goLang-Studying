56c843ac5bed03a20a582aedc431c1f38d61e04b
0noca <71095225+0noca@users.noreply.github.com>
2022-04-22 13:03:33 +0300

    manual commit

--- main.go
+++ main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world!")
+	var applicationName = "Application name"
+	const confTickets = 50
+	var remainingtTickets = confTickets
+
+	fmt.Printf("Welcome to %v booking application\n", applicationName)
+	fmt.Printf("We have a total of %v tickets  and %v is available\n", confTickets, remainingtTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	// ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("%v %v\n", userName, userTickets)
+
 }
